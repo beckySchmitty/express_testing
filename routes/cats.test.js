a8a54afb201dcaf18a6f9591a99c1177ef91b53a
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-16 14:46:20 -0500

    finished all testing routes

--- routes/cats.test.js
+++ routes/cats.test.js
@@ -17,18 +17,51 @@ afterEach(function () {
 
 describe("GET /cats", () => {
     test("get all cats", async () =>{
-        const resp = await request(app).get('/cats')
-        expect(resp.statusCode).toBe(200)
-        expect(resp.body).toEqual({cats:[rocket]})
+        const resp = await request(app).get('/cats');
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body).toEqual({cats:[rocket]});
+    })
+})
+
+describe("GET /cats", () => {
+    test("get cat by name", async () =>{
+        const resp = await request(app).get(`/cats/${rocket.name}`);
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body).toEqual({cat: rocket});
+    })
+    test("responds 404 for invalid cat", async () =>{
+        const resp = await request(app).get('/cats/badname');
+        expect(resp.statusCode).toBe(404);
     })
 })
 
 describe("POST /cats", () => {
     test("post new cat", async () =>{
-        const resp = await request(app).post('/cats').send({name: "Gus"})
-        expect(resp.statusCode).toBe(201)
-        expect(resp.body).toEqual({cat:{name: "Gus"}})
+        const resp = await request(app).post('/cats').send({name: "Gus"});
+        expect(resp.statusCode).toBe(201);
+        expect(resp.body).toEqual({cat:{name: "Gus"}});
     })
 })
+
+describe("PATCH to /cats", () => {
+    test("renaming rocket via patch request", async () => {
+        const resp = await request(app).patch(`/cats/${rocket.name}`).send({name: "pickles"});
+        expect(resp.statusCode).toEqual(200);
+        expect(resp.body).toEqual({cat:{name: "pickles"}});
+    })
+    test("404 after patch to incorrect name", async () => {
+        const resp = await request(app).patch('/cats/nocatnamedthis').send({name: "pickles"});
+        expect(resp.statusCode).toEqual(404);
+    })
+})
+
+describe("DELETE /cats/:name", function() {
+    test("Deletes a single a cat", async function() {
+      const resp = await request(app).delete(`/cats/${rocket.name}`);
+      expect(resp.statusCode).toBe(200);
+      expect(resp.body).toEqual({ message: "Deleted" });
+    });
+  });
+
 
 // remeber "request" is supertest
